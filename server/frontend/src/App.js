ff6e25ede81a853d947117446521d85ff89e03b2
Alex Davidson <alexdavidson1995@gmail.com>
2025-03-19 13:27:52 -0400

    Updated login, logout, register

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // Adjust the path based on your folder structure
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/Login" element={<LoginPanel />} />
+      <Route path="/Register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;

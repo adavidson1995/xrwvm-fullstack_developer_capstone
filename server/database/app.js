0c7c90c944f904db1b83768c545c401da5a0c274
Alex Davidson <112987190+adavidson1995@users.noreply.github.com>
2025-04-10 17:07:21 +0100

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -74,7 +74,7 @@ app.get('/fetchDealers', async (req, res) => {
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
   try {
-    const documents = await Dealerships.find({dealership: req.params.state});
+    const documents = await Dealerships.find({ state: req.params.state });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -86,7 +86,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
 try {
-  const documents = await Dealerships.find({dealership: req.params.id});
+  const documents = await Dealerships.find({ _id: req.params.id });
   res.json(documents);
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });

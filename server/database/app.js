f6376bf821c5268f28fd6ced7bed0271991f86ff
Alex Davidson <112987190+adavidson1995@users.noreply.github.com>
2025-04-10 14:54:16 +0100

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -36,7 +36,7 @@ app.get('/', async (req, res) => {
     res.send("Welcome to the Mongoose API")
 });
 
-// Express route to fetch all reviews
+// Express route to fetch all reviews (works)
 app.get('/fetchReviews', async (req, res) => {
   try {
     const documents = await Reviews.find();
@@ -46,7 +46,7 @@ app.get('/fetchReviews', async (req, res) => {
   }
 });
 
-// Express route to fetch reviews by a particular dealer
+// Express route to fetch reviews by a particular dealer (works)
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
   try {
     const documents = await Reviews.find({dealership: req.params.id});
@@ -56,19 +56,41 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
   }
 });
 
-// Express route to fetch all dealerships
+
+// Express route to fetch all dealerships (not working?)
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
-// Express route to fetch Dealers by a particular state
+
+
+// Express route to fetch Dealers by a particular state (not working)
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try {
+    const documents = await Dealerships.find({dealership: req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
+
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+  const documents = await Dealerships.find({dealership: req.params.id});
+  res.json(documents);
+} catch (error) {
+  res.status(500).json({ error: 'Error fetching documents' });
+}
 });
 
 //Express route to insert review
